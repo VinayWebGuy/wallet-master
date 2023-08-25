81c157249319dbcf084900dab6771365c8c7c8ec
Vinay <vinaywebguy@gmail.com>
2023-08-25 14:16:52 +0530

    Added login file

---
+++ login.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Setup - Wallet Master</title>
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <div class="theme-menu">
+        <i class="theme fa fa-moon"></i>
+    </div>
+    <div class="box">
+        <div class="logo">Wallet Master</div>
+
+        <div class="setup-form">
+            <h3>Login to your account first</h3>
+            <p>Get 0.5% interest of your total balance daily. *</p>
+            <form action="">
+            <div class="row">
+                <div class="form-grp">
+                    <label for="email">Email</label>
+                    <div class="input">
+                        <input type="email" name="email" autocomplete="off">
+                        <i class="fa fa-envelope input-icon"></i>
+                    </div>
+                </div>
+                <div class="form-grp">
+                    <label for="password">Password</label>
+                    <div class="input">
+                        <input type="password" name="password" autocomplete="off">
+                        <i class="fa fa-key input-icon"></i>
+                    </div>
+                </div>
+            </div>
+            <button>Login</button>
+            </form>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"
+        integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
+    <script src="assets/app.js"></script>
+</body>
+
+</html>
